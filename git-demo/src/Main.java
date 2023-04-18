a7e22468920fd73aea5aca2313c2361ba7110005
Aiu <554010195@qq.com>
2023-04-18 19:02:06 +0800

    push test
    Signed-off-by: Aiu <554010195@qq.com>

--- git-demo/src/Main.java
+++ git-demo/src/Main.java
@@ -1,8 +1,7 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("hot-fix");
+        System.out.println("push test");
 
     }
 }
